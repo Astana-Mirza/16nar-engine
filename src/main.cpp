f201ea4cadff2b5a9ffff544e1425190529a8376
Astana-Mirza <lyulin.2003@mail.ru>
2022-01-23 16:16:17 +0300

    delete old files

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "../inc/game.h"
-
-#define SFML_DEFINE_DISCRETE_GPU_PREFERENCE
-
-int main() {
-	_16nar::Game::get_game().load_scene("main_menu.nar");
-	_16nar::Game::get_game().run();
-	return 0;
-}
